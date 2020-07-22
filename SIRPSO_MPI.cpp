2b4898ff104ee21290d4218665e1f36482c5864c
John Whitman <whitman.103@osu.edu>
2020-07-22 02:38:36 -0400

    script

--- SIRPSO_MPI.cpp
+++ SIRPSO_MPI.cpp
@@ -1,4 +1,3 @@
-#include <windows.h>
 #include <chrono>
 #include <ctime>
 #include <map>
---
+++ runScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --ntasks=20
+#SBATCH --nodes=1
+
+module load gcc/7.2.0
+module load openmpi-1.8/gcc
+mpirun -np 20 ./threeParams.exe "20"
