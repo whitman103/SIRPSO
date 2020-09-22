804a3d29efb2562bce0ba877b7f3c0f02a84d3aa
John Whitman <whitman.103@osu.edu>
2020-09-22 16:51:00 -0400

    script update

--- pVav_runScript.sh
+++ pVav_runScript.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-#SBATCH --ntasks=3
-#SBATCH -n 60
 
-mpirun -np 20 ./pVav_MPI_means.exe "20" "InputFolder//outputOne.txt" "0" &
-mpirun -np 20 ./pVav_MPI_means.exe "20" "InputFolder//outputTwo.txt" "1" &
-mpirun -np 20 ./pVav_MPI_means.exe "20" "InputFolder//outputThree.txt" "2" &
+sbatch pVav_runScript1.sh
+sbatch pVav_runScript2.sh
+sbatch pVav_runScript3.sh
 
-wait
