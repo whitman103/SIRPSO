9c5a6a5148df7c97673940c26a6926d3e2efcd02
whitman103 <64374101+whitman103@users.noreply.github.com>
2020-11-30 02:37:18 -0500

    update

--- pVav_ODE_runScript.sh
+++ pVav_ODE_runScript.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-load module GCC/9.3.0
-load module GCC/9.3.0 OpenMPI/4.0.3 
-load module GCC/9.3.0 OpenMPI/4.0.3 Boost/1.72.0
 sbatch pVav_ODE_runScript1.sh
 sbatch pVav_ODE_runScript2.sh
 sbatch pVav_ODE_runScript3.sh
---
+++ prepEnvironment.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+module load GCC/9.3.0
+module load GCC/9.3.0 OpenMPI/4.0.3 
+module load GCC/9.3.0 OpenMPI/4.0.3 Boost/1.72.0
